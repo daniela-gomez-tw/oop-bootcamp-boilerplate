9152eb6f543e7f9a455483baef1df1992b4e38a3
luso97 <=>
2023-02-10 12:12:55 +0100

    decode from NATO and refactor

--- src/main/java/oop/nato/NatoTranslator.java
+++ src/main/java/oop/nato/NatoTranslator.java
@@ -1,70 +1,82 @@
 package oop.nato;
 
-import java.util.Map;
-import java.util.Optional;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.util.Map.entry;
-
 public class NatoTranslator {
-    private static final Map<String, String> natoAlphabet = Map.ofEntries(
-            entry("a", "alpha"),
-            entry("b", "bravo"),
-            entry("c", "charlie"),
-            entry("d", "delta"),
-            entry("e", "echo"),
-            entry("f", "foxtrot"),
-            entry("g", "golf"),
-            entry("h", "hotel"),
-            entry("i", "india"),
-            entry("j", "juliett"),
-            entry("k", "kilo"),
-            entry("l", "lima"),
-            entry("m", "mike"),
-            entry("n", "november"),
-            entry("o", "oscar"),
-            entry("p", "papa"),
-            entry("q", "quebec"),
-            entry("r", "romeo"),
-            entry("s", "sierra"),
-            entry("t", "tango"),
-            entry("u", "uniform"),
-            entry("v", "victor"),
-            entry("w", "whiskey"),
-            entry("x", "xray"),
-            entry("y", "yankee"),
-            entry("z", "zulu"),
-            entry("1", "one"),
-            entry("2", "two"),
-            entry("3", "three"),
-            entry("4", "four"),
-            entry("5", "five"),
-            entry("6", "six"),
-            entry("7", "seven"),
-            entry("8", "eight"),
-            entry("9", "nine"),
-            entry("0", "zero"),
-            entry(" ", "null")
-    );
-    public static String encodeToNato(String phrase) {
-        return transformToStreamOfLetters(phrase)
-            .map(encodeLetterToNato())
+    private static final BiMap<String, String> natoAlphabet = HashBiMap.create();
+
+    public static String encode(String phrase) {
+        return transformToStreamOfLetters(phrase, "")
+            .map(encodeLetter())
             .reduce(concatWithHyphen())
             .get();
     }
 
-    private static Stream<String> transformToStreamOfLetters(String phrase) {
-        return Stream.of(phrase.split(""));
+    public static String decode(String phrase) {
+        return transformToStreamOfLetters(phrase, "-")
+            .map(decodeLetter())
+            .reduce(String::concat)
+            .get();
     }
 
-    private static Function<String, String> encodeLetterToNato() {
-        return letter -> natoAlphabet.get(letter);
+    private static Function<String, String> decodeLetter() {
+        return letter -> natoAlphabet.inverse().get(letter);
+    }
+
+    private static Function<String, String> encodeLetter() {
+        return natoAlphabet::get;
     }
 
     private static BinaryOperator<String> concatWithHyphen() {
         return (accumulated, newLetter) -> accumulated.concat("-" + newLetter);
+    }
+
+    private static Stream<String> transformToStreamOfLetters(String phrase, String regex) {
+        return Stream.of(phrase.split(regex));
+    }
+
+    static{
+        natoAlphabet.put("a", "alpha");
+        natoAlphabet.put("b", "bravo");
+        natoAlphabet.put("c", "charlie");
+        natoAlphabet.put("d", "delta");
+        natoAlphabet.put("e", "echo");
+        natoAlphabet.put("f", "foxtrot");
+        natoAlphabet.put("g", "golf");
+        natoAlphabet.put("h", "hotel");
+        natoAlphabet.put("i", "india");
+        natoAlphabet.put("j", "juliett");
+        natoAlphabet.put("k", "kilo");
+        natoAlphabet.put("l", "lima");
+        natoAlphabet.put("m", "mike");
+        natoAlphabet.put("n", "november");
+        natoAlphabet.put("o", "oscar");
+        natoAlphabet.put("p", "papa");
+        natoAlphabet.put("q", "quebec");
+        natoAlphabet.put("r", "romeo");
+        natoAlphabet.put("s", "sierra");
+        natoAlphabet.put("t", "tango");
+        natoAlphabet.put("u", "uniform");
+        natoAlphabet.put("v", "victor");
+        natoAlphabet.put("w", "whiskey");
+        natoAlphabet.put("x", "xray");
+        natoAlphabet.put("y", "yankee");
+        natoAlphabet.put("z", "zulu");
+        natoAlphabet.put("1", "one");
+        natoAlphabet.put("2", "two");
+        natoAlphabet.put("3", "three");
+        natoAlphabet.put("4", "four");
+        natoAlphabet.put("5", "five");
+        natoAlphabet.put("6", "six");
+        natoAlphabet.put("7", "seven");
+        natoAlphabet.put("8", "eight");
+        natoAlphabet.put("9", "nine");
+        natoAlphabet.put("0", "zero");
+        natoAlphabet.put(" ", "null");
     }
 }
--- src/test/java/oop/nato/NatoTranslatorTest.java
+++ src/test/java/oop/nato/NatoTranslatorTest.java
@@ -7,12 +7,23 @@ import static org.testng.Assert.assertEquals;
 public class NatoTranslatorTest {
 
     @Test
-    public void itShouldTranslateToNatoSingleChar() {
-        assertEquals("hotel", NatoTranslator.encodeToNato("h"));
+    public void itShouldTranslateToNatoSingleLetter() {
+        assertEquals("hotel", NatoTranslator.encode("h"));
     }
 
     @Test
     public void itShouldTranslateToNatoWord() {
-        assertEquals("hotel-echo-lima-lima-oscar", NatoTranslator.encodeToNato("hello"));
+        assertEquals("hotel-echo-lima-lima-oscar", NatoTranslator.encode("hello"));
+    }
+
+    @Test
+    public void itShouldTranslateFromNatoWord() {
+        assertEquals("hello", NatoTranslator.decode("hotel-echo-lima-lima-oscar"));
+    }
+
+    @Test
+    public void itShouldTranslateSentenceToNato() {
+        assertEquals("kilo-hotel-alpha-bravo-india-bravo-null-november-uniform-romeo-mike-alpha-golf-oscar-mike-echo-delta-oscar-victor",
+                NatoTranslator.encode(""));
     }
 }
